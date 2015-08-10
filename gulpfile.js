abf65de233b71dba040d5c4464043b8ab88ed7ec
Zak Stallings <zakstal@gmail.com>
2015-08-09 21:02:58 -0400

    add dependencies for css

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,77 @@
 var gulp = require('gulp');
+var pkg = require('./package.json');
 var jade = require('gulp-jade');
 var uglify = require('gulp-uglify');
 var util = require('gulp-util');
 var $ = require('gulp-load-plugins')();
 
-console.log('here');
+var AUTOPREFIXER_BROWSERS = [
+    'ie >= 9',
+    'ie_mob >= 9',
+    'chrome >= 39',
+    'and_chr >= 39',
+    'safari >= 6.1',
+    'ff >= 34',
+    'ios >= 6.1',
+    'android >= 4'
+];
+
+var currentDate = new Date();
+
+var year = currentDate.getFullYear();
+
+var formattedTime = currentDate.toLocaleTimeString(currentDate);
+
+var datetime = currentDate.getMonth() + 1 + '/'
+    + currentDate.getDate() + '/'
+    + currentDate.getFullYear() + ' @ '
+    + formattedTime;
+
+var copyright = '* Copyright (c) ' + year + ' MemoryGame';
+var version = '* @version v' + pkg.version;
+var buildDate = '* File built on: ' + datetime;
+
+var cvb = [
+		' ' + copyright,
+    ' ' + version,
+    ' ' + buildDate].join('\n');
+
+var htmlBanner = ['<!--',
+    cvb,
+    ' -->',
+    ''].join('\n');
+
+var jsBanner = ['\n', '/*!',
+    cvb,
+    ' */',
+    ''].join('\n');
+
+var cssBanner = ['/*!***********************************',
+    cvb,
+    ' *************************************/',
+    ''].join('\n');
+
+function onError (err) {
+    util.log(util.colors.red(err.message));
+
+    $.notify.onError({
+        title: 'Compilation Failure!',
+        message: 'Check your terminal console',
+        sound: 'Basso'
+    })(err);
+
+    this.emit('end');
+}
  
 gulp.task('templates', function() {
+	util.log(util.colors.bgGreen.bold('Compiling jade tempates'));
   gulp.src('./src/**/*.jade')
     .pipe(jade())
     .pipe(gulp.dest('./dist/'))
 });
  
 gulp.task('compress', function() {
+  util.log(util.colors.bgGreen.bold('Concatenate & minify your JS'));
   return gulp.src('src/**/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('dist/js'));
@@ -46,6 +105,29 @@ gulp.task('vendor-scripts', function () {
         .pipe($.size({title: 'vendor scripts'}));
 });
 
+// Compile, auto-prefix, concatenate & minify stylesheets
+gulp.task('styles', function () {
+    util.log(util.colors.bgGreen.bold('Compiling LESS --> CSS'));
+
+    return gulp.src([
+        './src/less/global-imports.less',
+        './src/components/**/*.less'
+    ]).pipe($.plumber({
+        errorHandler: onError
+    }))
+        .pipe($.sourcemaps.init())
+        .pipe(gulp.dest('src/less'))
+        .pipe($.less())
+        .pipe($.autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
+        .pipe($.minifyCss())
+        .pipe($.wrapper({
+            footer: cssBanner
+        }))
+        .pipe($.sourcemaps.write('.'))
+        .pipe(gulp.dest('dest/css'))
+        .pipe($.size({title: 'styles'}));
+});
+
 gulp.task('watch', function () {
     util.log(util.colors.bgGreen.bold('Gulp is now watching for changes!'));
 
@@ -56,6 +138,6 @@ gulp.task('watch', function () {
     gulp.watch(['dist/pages/index.jade', 'dist/js/*.js', 'dist/css/*.css']);
 });
 
-gulp.task('default', ['templates', 'vendor-scripts', 'compress', 'watch']);
+gulp.task('default', ['templates', 'styles', 'vendor-scripts', 'compress', 'watch']);
 
 
